31f0590fdde468b8a1998f9be98adbb6dcf3821f
Yura <yurgayura44@gmail.com>
2021-10-20 20:57:58 +0300

    Different styles for different items using DOM

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// h1 - element;
+const elementBig = document.querySelector('h1').style.backgroundColor = "#90EE90";
+
+
+
+// id="myDiv" - fisrt element;
+const elementDivFirst = document.getElementById('myDiv').children[0].style.fontWeight = "bold";
+
+// id="myDiv" - second element;
+const elementDivSecond = document.getElementById('myDiv').children[1].style.color = "red";
+
+// id="myDiv" - third element;
+const elementDivThird = document.getElementById('myDiv').children[2].style.textDecoration = "underline";
+
+// id="myDiv" - fourth element;
+const elementDivFourth = document.getElementById('myDiv').children[3].style.fontStyle = "italic";
+
+
+
+// id="myList" - first list;
+const elementsListFirst = document.getElementById('myList').children[0].style.display = "inline";
+
+// id="myList" - second list;
+const elementsListSecond = document.getElementById('myList').children[1].style.display = "inline";
+
+// id="myList" - second list;
+const elementsListThird = document.getElementById('myList').children[2].style.display = "inline";
+
+
+
+// element visibility;
+const elementVisabl = document.querySelector('span').style.visibility = "hidden";
